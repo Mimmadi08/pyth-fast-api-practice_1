6fef640219b44deb067e187650956fa188d0488d
CharitIT <immadi.charit@gmail.com>
2026-05-31 16:53:57 -0400

    .

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 
+# This creates your API application
 app = FastAPI()
 
+# @app.get("/") means:
+# when someone visits the "/" URL, run this function
 @app.get("/")
 def hello():
- return {"message": "This your first API call, and it is working!"}
+    return {"message": "My first API is working!"}
 
+# A route with a variable in the URL
+# whatever name you type in the URL comes in as 'name'
 @app.get("/greet/{name}")
-def greet(name :str):
-   return {"hello ":name}
+def greet(name: str):
+    return {"hello": name}
